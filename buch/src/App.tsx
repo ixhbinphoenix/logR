d688cd5a939803bf2827bcdbfddb1502552f1c5c
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2021-09-20 19:57:08 +0200

    Basic Project Structure

---
+++ buch/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Logr</p>
+        <p>
+          Made with React and Vite
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
